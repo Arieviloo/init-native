017ef6ed7a58e4dd3436b3767fe7c5935188e633
Jadiê Oliveira <jadieoliveira00@gmail.com>
2020-05-29 00:38:22 -0300

    init

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <Text>Meu App</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: "#f00",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
