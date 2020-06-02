126840e841b40c7c80e9abb9c54fb5832b272baa
Jadiê <jadieoliveira00@gmail.com>
2020-06-01 22:35:10 -0300

    imc

--- App.js
+++ App.js
@@ -1,44 +1,63 @@
-import React, {useState} from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
-  const [peso, setPeso] = useState('');
-  const [altura, setAltura] = useState('');
+  const [peso, setPeso] = useState("");
+  const [altura, setAltura] = useState("");
+
+  const handleSubimit = () => {
+    const valorALtura = altura / 100;
+    const imc = peso / (valorALtura * valorALtura);
+
+    if (imc <= 18.6) {
+      alert(`${imc.toFixed(2)} Você está abaixo do peso`);
+    } else if (imc >= 18.6 && imc < 24.9) {
+      alert(`${imc.toFixed(2)} Você está com o peso ideal`);
+    } else if (imc >= 24.9) {
+      alert(`${imc.toFixed(2)} está acima do peso`);
+    }
+  };
   return (
     <View style={styles.container}>
-       <Text style={styles.title}>Calcule seu IMC</Text>
-       <TextInput 
+      <Text style={styles.title}>Calcule seu IMC</Text>
+      <TextInput
         style={styles.input}
         placeholder="Peso (kg)"
         value={peso}
-        onChangeText={peso => setPeso(peso)}
+        onChangeText={(peso) => setPeso(peso)}
         keyboardType="numeric"
-       />
-       <TextInput 
+      />
+      <TextInput
         style={styles.input}
         placeholder="Altura (cm)"
         value={altura}
-        onChangeText={altura => setAltura(altura)}
+        onChangeText={(altura) => setAltura(altura)}
         keyboardType="numeric"
-       />
-       <TouchableOpacity style={styles.button}>
-         <Text style={styles.buttonText}>Calcular</Text>
-       </TouchableOpacity>
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSubimit}>
+        <Text style={styles.buttonText}>Calcular</Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#7159c3',
-    justifyContent: 'center',
-    alignContent: 'center'
+    backgroundColor: "#7159c3",
+    justifyContent: "center",
+    alignContent: "center",
   },
   title: {
-    textAlign: 'center',
+    textAlign: "center",
     marginTop: 30,
-    fontSize: 30
+    fontSize: 30,
   },
   input: {
     backgroundColor: "#333",
@@ -47,11 +66,11 @@ const styles = StyleSheet.create({
     paddingLeft: 15,
     borderRadius: 50,
     fontSize: 20,
-    color: "#fff"
+    color: "#fff",
   },
   button: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     margin: 15,
     backgroundColor: "#444",
     padding: 10,
@@ -59,6 +78,6 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     fontSize: 20,
-    color: "#fff"
-  }
-})
+    color: "#fff",
+  },
+});
